ed79084afd94a4efb5086cfd21f9b439360d20bb
JYOTHIKULAL <jyothikulal007@gmail.com>
2026-02-03 11:48:29 +0530

    abcd

---
+++ src/inter2.py
@@ -0,0 +1,11 @@
+age=22
+height=5.4
+name='jyothi'
+is_student=True
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(is_student))
+
+
+
---
+++ src/inter3.py
@@ -0,0 +1,4 @@
+num1=input("enter the number:")
+num2=input("enter the second number:")
+sum=float(num1)+float(num2)
+print("the sum of {0} and {1} is  {2}",num1,num2,sum)
