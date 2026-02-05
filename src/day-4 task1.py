d0064130d496ec9b6ce5eb3a29796ed9e5e79342
JYOTHIKULAL <jyothikulal007@gmail.com>
2026-02-05 10:42:52 +0530

    Add contact dictionary with safe lookup and print

---
+++ src/day-4 task1.py
@@ -0,0 +1,22 @@
+# Step 1: Create a dictionary with at least three contacts
+contacts = {
+    "Jyothi": "8746001575",
+    "Hitha": "9591607129",
+    "Charlie": "9001122334"
+}
+
+contacts["Diana"] = "9988776655"
+
+contacts["Hitha"] = "9591607129"
+
+existing_contact = contacts.get("jyothi", "Contact not found")
+missing_contact = contacts.get("Eve", "Contact not found")
+
+print("Safe Lookup Results:")
+print("Jyothi:", existing_contact)
+print("Eve:", missing_contact)
+
+print("\nContact List:")
+
+for name, phone in contacts.items():
+    print(f"Contact: {name} | Phone: {phone}")
